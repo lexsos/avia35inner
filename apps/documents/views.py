219a08d18eb3192231707167dd4bd17cea8e8038
lexsos <ale-sosnin@yandex.ru>
2014-01-23 10:30:02 +0400

    Fix app documents

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -13,7 +13,7 @@ class PageListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(PageListView, self).get_context_data(**kwargs)
         context['document'] = get_object_or_404(
-            Document,
+            Document.objects.published(),
             pk=self.kwargs['pk']
         )
         return context
